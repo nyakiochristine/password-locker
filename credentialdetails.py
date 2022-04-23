9d44adf5f183e18f23d1237397f3b1c599cb507b
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-23 23:42:22 +0300

    ft:created a class for generating new instances for users credentials

---
+++ credentialdetails.py
@@ -0,0 +1,22 @@
+import string
+import random
+
+class credential:
+    """
+    Generates new instances of user credential details
+    
+    """
+    cred_list = []
+    
+    def __init__(self,account_name,username,password):
+        """
+        oobject properties defined
+        Args:
+            account_name:new account name
+            username:new username
+            password:new password
+        """
+        
+        
+    
+    

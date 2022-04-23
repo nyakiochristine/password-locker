de4dec2423a7fb7bb5753b32630024fbfc52b86f
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-23 22:57:01 +0300

    patch:fixed errors

--- user.py
+++ user.py
@@ -15,7 +15,7 @@ class User:
             last_name: New user last name.
             username: New user username.
             password: New user password.
-     """"
+     """
      self.first_name = first_name
      self.last_name = last_name
      self.username = username

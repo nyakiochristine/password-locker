721190a87273af1c7e7828b3ef3efe9717e648c1
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-23 23:12:07 +0300

    ft:added methods to test if user info is saved and aslo wheter it exists

--- user-test.py
+++ user-test.py
@@ -14,12 +14,32 @@ class TestUser(unittest.TestCase):
         """
         method running before tests are run
         """
-        self.new_user = ('Chris', 'Mwangi', 'kriss')
+        self.new_user = ('Chris', 'Mwangi','cream-ks', 'kriss')
         
         
         def test_init(self):
-            "checking the initialization"
+            """
+            method used to check if the initialization is successful
+            """
             self.assertEqual(self.new_user.first_name, 'Chris')
             self.assertEqual(self.new_user.last_name, 'Mwangi')
+            self.assertEqual(self.new_user.username, 'cream-ks')
             self.assertEqual(self.new_user.password, 'kriss')
             
+        def test_save_user(self):
+            """
+            check if the user info is saved
+            """
+            self.new_user.save_user = ()
+            self.assertEqual(len(User.user_list),1)
+            
+        def test_user_exists(self):
+            """
+            checking if information abt users exists
+            """
+            self.new_user.save_user()
+            self.assertTrue(User.user_exists('cream-ks'))
+
+if __name__ == '__main__':
+    unittest.main()
+

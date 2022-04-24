2e4524f2ad030de7093dfb91f34fa8d557271800
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-24 15:09:07 +0300

    ft:added main function

---
+++ run.py
@@ -0,0 +1,24 @@
+import unittest
+from user import User
+from credentialdetails import Credential
+import random
+import string
+
+def main():
+        """
+        runs before other functions
+        """
+        while True:
+            selector = input(
+                "Welcome to this password manager! Type in the following:",
+            )
+            
+        
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+

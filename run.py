b082f9091f354a4476846fc0ef70a42f14df1941
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-04-24 22:28:52 +0300

    ft:added functions for saving and displaying credentials

--- run.py
+++ run.py
@@ -31,16 +31,40 @@ def login_user():
     else:
         print("Username or password is does not exist. Please.")
         anykey = input("Enter your password and username")
-        return false
+        return False
+    
+    def make_credentials(account_name,username, password):
+        """
+        make new credentials
+
+        """
+        save_credentials(Credential(account_name,username,password))
+        
+        
+    def save_credentials(creds):
+        """
+        stores credentials in the database
+        """
+        Credential.cred_list.append(creds)
+        
+        
+    ef display_credentials():
+        """
+        method to display credentials
+        """
+
+        
+        
+        
+       
+        
 
 def main():
         """
         runs before other functions
         """
         while True:
-            selector = input(
-                "Welcome to this password manager! Type in the following:",
-            )
+            selector = input("Welcome to this password manager! Type in the following")
             
         
 
